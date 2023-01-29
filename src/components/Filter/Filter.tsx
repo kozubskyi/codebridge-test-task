3646ba23bd9ee6a4e9b32c7871cbdf81417754a4
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-01-30 00:02:35 +0200

    bugfix

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,7 +2,7 @@ import { FC, ChangeEvent, useMemo } from 'react'
 import { TextField, Box } from '@mui/material'
 import debounce from 'lodash.debounce'
 import './Filter.scss'
-import { useAppSelector, useAppDispatch } from '../../store/hooks'
+import { useAppDispatch } from '../../store/hooks'
 import { fetchArticles } from '../../services/spaceflight-news-api'
 import { filterActions } from '../../store/slices/filter-slice'
 import SearchIcon from '../icons/SearchIcon/SearchIcon'
@@ -11,13 +11,15 @@ const Filter: FC = () => {
 	// const filter = useAppSelector(state => state.filter)
 	const dispatch = useAppDispatch()
 
-	const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
-		const { value } = evt.target
-		dispatch(filterActions.typing(value))
-		dispatch(fetchArticles(value))
-	}
-
-	const debouncedOnChange = useMemo(() => debounce(onChange, 300), [])
+	const onChange = useMemo(
+		() =>
+			debounce((evt: ChangeEvent<HTMLInputElement>) => {
+				const { value } = evt.target
+				dispatch(filterActions.typing(value))
+				dispatch(fetchArticles(value))
+			}, 300),
+		[dispatch]
+	)
 
 	return (
 		<Box
@@ -33,7 +35,7 @@ const Filter: FC = () => {
 					size="small"
 					fullWidth
 					// value={filter}
-					onChange={debouncedOnChange}
+					onChange={onChange}
 					sx={{ fontFamily: 'Montserrat' }}
 				/>
 			</Box>
